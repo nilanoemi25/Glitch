2d6c999d77114483a9792cdc5b42a252fb925146
Glitch (aquatic-peat-washer) <none>
2024-06-22 11:08:26 +0000

    📰🌔 Checkpoint ./routes.js:68361383/390 ./views/partials/menu.hbs:68361383/68 ./views/partials/list-playlists.hbs:68361383/10

--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { playlistController } from "./controllers/playlist-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
+import { accountsController } from './controllers/accounts-controller.js';
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+/*
+router.get("/", dashboardController.index); */
 router.get("/dashboard", dashboardController.index);
 router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
 router.get("/playlist/:id", playlistController.index);
@@ -13,3 +15,9 @@ router.post("/playlist/:id/addtrack", playlistController.addTrack);
 router.get("/about", aboutController.index);
 router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
